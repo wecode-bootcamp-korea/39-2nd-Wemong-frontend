b64f99af758d4977685685217fb711cb9df7aa18
unknown <hsh1510@gmail.com>
2022-11-28 21:16:56 +0900

    ADD:기본 LoginRayout 구성

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <LoginBox>
+      <IdInput placeholder="이메일 입력" />
+      <IdInput placeholder="비밀번호 입력" />
+      <KakaoLogin>카카오 로그인</KakaoLogin>
+    </LoginBox>
+  );
 };
+
+const LoginBox = styled.div`
+  width: 400px;
+  height: 400px;
+  background-color: blue;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const IdInput = styled.input`
+  width: 200px;
+  height: 30px;
+`;
+
+const KakaoLogin = styled.button`
+  width: 200px;
+  height: 40px;
+  background-color: yellow;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 export default LoginPage;
