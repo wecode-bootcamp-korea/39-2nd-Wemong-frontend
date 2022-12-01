ac211ad37a8743dc541df943aa39c5de831dcf8f
SangYoonLee <prestro1231@hufs.ac.kr>
2022-12-01 17:52:51 +0900

    test

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage/MainPage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import SignUpPage from './pages/SignUpPage/SignupPage';
 import DetailPage from './pages/DetailPage/DetailPage';
